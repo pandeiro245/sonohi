9893b1410fe7e5cde680920a73e7d85d34d2b559
pandeiro245 <nishiko@mindia.jp>
2018-07-14 21:49:23 +0900

    Facebook Login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="home">
   <h3>{{ msg }}</h3>
-  <button @click="googleLogin">Googleログイン</button>
-  <button @click="twitterLogin">Twitterログイン</button>
+  <button @click="googleLogin">Google</button>
+  <button @click="twitterLogin">Twitter</button>
+  <!--<button @click="githubLogin">GitHub</button>-->
+  <button @click="facebookLogin">Facebook</button>
   </div>
 </template>
 <script>
@@ -19,6 +21,12 @@
     },
     twitterLogin: function(){
       firebase.auth().signInWithRedirect(new firebase.auth.TwitterAuthProvider());
+    },
+    githubLogin: function(){
+      firebase.auth().signInWithRedirect(new firebase.auth.GithubAuthProvider());
+    },
+    facebookLogin: function(){
+      firebase.auth().signInWithRedirect(new firebase.auth.FacebookAuthProvider());
     },
   }
 }
