662e54550991185fdb24cae6a6f2151921d12b81
pandeiro245 <nishiko@mindia.jp>
2018-07-16 16:45:54 +0900

    add a component of Visions

--- src/App.vue
+++ src/App.vue
@@ -2,74 +2,22 @@
 <div id="app">
 <a href='/'><img src="https://ruffnote.com/attachments/56117" width="320px" /></a>
 <h1>{{ msg }}</h1>
-<p>2018年7月16日からあと{{remain}}日。</p>
-<ul>
-<a href='#20180715'><<前の日</a>
-<a href='#20180715'>次の日>></a>
-<a href='#20180715'><<前の月</a>
-<a href='#20180715'>次の月>></a>
-<a href='#20180715'><<前の年</a>
-<a href='#20180715'>次の年>></a>
-</ul>
+
 <p>このサイトは西小倉宏信が<br>その日までに3つの目標を達成するために<br />
 たくさんの人を巻き込むことを目的としています。</p>
 
 
-<h3>1. 10万人からの「ありがとう」を集める</h3>
-<table class='table table-bordered'>
-<tr>
-<th>2019</th>
-<th>2020</th>
-<th>2021</th>
-<th>2022</th>
-</tr>
-<tr>
-<td>0 / 20,000</td>
-<td>0 / 40,000</td>
-<td>0 / 60,000</td>
-<td>0 / 100,000</td>
-</tr>
-</table>
-<h3>2. 100人の年収を1,000万円以上にする</h3>
-<p>雇用形態（正社員・業務委託）は問わない</p>
-<table class='table table-bordered'>
-<tr>
-<th>2019</th>
-<th>2020</th>
-<th>2021</th>
-<th>2022</th>
-
-</tr>
-<tr>
-<td>0 / 3</td>
-<td>0 / 30</td>
-<td>0 / 70</td>
-<td>0 / 100</td>
-</tr>
-</table>
-
-<h3>3. 47都道府県に拠点を持つ</h3>
-<table class='table table-bordered'>
-<tr>
-<th>2019</th>
-<th>2020</th>
-<th>2021</th>
-<th>2022</th>
-</tr>
-<tr>
-<td>4 / 10</td>
-<td>4 / 30</td>
-<td>4 / 40</td>
-<td>4 / 47</td>
-</tr>
-</table>
 
 <Home v-if="!isLogin"></Home>
 <Dashboard v-if="isLogin" :user="userData"></Dashboard>
+<Visions></Visions>
+
 </div>
+
 </template>
 
 <script>
+import Visions from './components/Visions.vue';
 import Home from './components/Home.vue';
 import Dashboard from './components/Dashboard.vue';
 
@@ -88,8 +36,9 @@ export default {
     }
   },
   components: {
+    'Visions': Visions,
     'Home': Home,
-    'Dashboard': Dashboard
+    'Dashboard': Dashboard,
   },
 	created: function() {
 		firebase.auth().onAuthStateChanged(user => {
